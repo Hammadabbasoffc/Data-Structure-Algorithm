c4115a2fa67078decd1080035931728387a418e7
Hammad Abbas <hammadabbasoffc@gmail.com>
2023-11-08 16:21:07 +0500

    On branch main Your branch is up to date with 'origin/main'.

--- binarySearch/firstAndLast__Occurance.cpp
+++ binarySearch/firstAndLast__Occurance.cpp
@@ -13,18 +13,43 @@ int firstOccurance(int arr[] , int n , int key){
         }else if (key > arr[mid] )
         {
             s = mid +1;
-        }else{
+        }else if (key < arr[mid])
+        {
             e = mid -1;
         } 
-        int mid = s + (e-s)/2;
+        
+        mid = s + (e-s)/2;
+    }
+
+    return ans;
+}
+
+int lastOccurance(int arr[] , int n , int key){
+    int s = 0 , e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+
+    while( s <= e){
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }else if (key > arr[mid] )
+        {
+            s = mid +1;
+        }else if (key < arr[mid])
+        {
+            e = mid -1;
+        } 
+        
+        mid = s + (e-s)/2;
     }
 
     return ans;
 }
 int main(){
 
-    int even[5] = {1,2,3,4,5};
-    cout<<"First occurence is "<<firstOccurance(even , 5 , 3) <<endl;
+    int even[5] = {3,2,3,3,3};
+    cout<<"First occurence is "<<firstOccurance(even , 5 , 3) << " Last occurence is "<<lastOccurance(even , 5 , 3) <<endl;
 
 return 0;
 }
