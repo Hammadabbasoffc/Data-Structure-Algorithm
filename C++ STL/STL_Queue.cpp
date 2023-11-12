cc3c3c7e3c81353a46168b36b59fca9908f0483b
Hammad Abbas <hammadabbasoffc@gmail.com>
2023-11-12 19:36:33 +0500

    On branch main Your branch is up to date with 'origin/main'.

--- C++ STL/STL_Queue.cpp
+++ C++ STL/STL_Queue.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 int main(){
 
+queue <string> q;
 
+q.push("Hammad");
+q.push("Abbas");
+q.push("Love Coding");
+
+cout<<"First Element -> " <<q.front()<<endl;
+cout<<"Size before Pop-> "<<q.size()<<endl;
+
+q.pop();
+
+cout<<"First Element -> " <<q.front()<<endl;
+cout<<"Size After Pop-> "<<q.size()<<endl;
 
 return 0;
 }
