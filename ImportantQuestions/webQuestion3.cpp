57b859abfae180b816596fd6059a6cb3e8581524
Hammad Abbas <hammadabbasoffc@gmail.com>
2023-11-13 18:52:16 +0500

    On branch main Your branch is up to date with 'origin/main'.

---
+++ ImportantQuestions/webQuestion3.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/rotate-array/submissions/1097847674/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> temp(nums.size());
+
+        for(int i = 0; i<nums.size(); i++){
+            temp[(i+k)%nums.size()] = nums[i];
+        }
+        nums = temp;
+    }
+};
