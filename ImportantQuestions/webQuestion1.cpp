c9d5ddc58d5218262af1c661e3b81b633ca064c9
Hammad Abbas <hammadabbasoffc@gmail.com>
2023-11-13 15:42:00 +0500

    On branch main Your branch is up to date with 'origin/main'.

---
+++ ImportantQuestions/webQuestion1.cpp
@@ -0,0 +1,17 @@
+/*
+https://leetcode.com/problems/merge-sorted-array/submissions/1097821954/
+*/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+       for(int i = m , j = 0; j<n ; j++){
+           nums1[i] = nums2[j];
+           i++;
+       } 
+
+    sort(nums1.begin() , nums1.end());
+
+    }
+};
