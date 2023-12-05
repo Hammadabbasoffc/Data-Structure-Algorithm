cee622630dff694ff018c44c626b00752f6ab92e
Hammad Abbas <hammadabbasoffc@gmail.com>
2023-12-05 19:19:25 +0500

    On branch main Your branch is up to date with 'origin/main'.

---
+++ Recursion/power.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int power(int n){
+    if(n == 0){
+        return 1;
+    }
+
+    return 2 * power(n-1);
+}
+
+int main(){
+
+int n;
+cout<<"Enter the number : ";
+cin>>n;
+
+int ans = power(n);
+cout<<"Answer is : "<<ans<<endl;
+return 0;
+}
